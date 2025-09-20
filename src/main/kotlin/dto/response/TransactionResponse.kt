080171e079253c181a428d449c3ac116484ac282
Chukwuemeka Chukwulobe <cchukwulobe@moniepoint.com>
2025-09-20 10:35:33 +0100

    Formatted the DTO codes

--- src/main/kotlin/dto/response/TransactionResponse.kt
+++ src/main/kotlin/dto/response/TransactionResponse.kt
@@ -7,18 +7,25 @@ import java.math.BigInteger
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class TransactionResponse(
+
     @param:JsonProperty("transaction_id")
     val transactionId: String,
+
     @param:JsonProperty("amount")
     val amount: BigInteger,
+
     @param:JsonProperty("asset")
     val asset: String,
+
     @param:JsonProperty("type")
     val type: String,
+
     @param:JsonProperty("fee")
     val fee: BigInteger,
+
     @param:JsonProperty("rate")
     val rate: BigDecimal?,
+
     @param:JsonProperty("description")
     val description: String
 )
